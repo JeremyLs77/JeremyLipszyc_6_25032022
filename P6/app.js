31e993b748559190d19d7ac8d1867bd91543a909
Jeremy Ls <jldev8315@gmail.com>
2022-03-25 14:43:04 +0100

    git init P6

---
+++ P6/app.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+mongoose.connect('mongodb+srv://jldev:Tcv3TPz3TKdgw2Es@cluster0.z07yk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+module.exports = app;
